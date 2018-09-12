62cc8aba94294bc24427efb10f049ad663b87020
diego0722 <diego3b2ecu@gmail.com>
2018-09-12 16:09:47 -0500

    Lago Musical

---
+++ Sonidos/Canciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonidos
+{
+    class Canciones
+    {
+        //0->Canción 1, 1->Canción 2, 2->Canción 3
+        string[] Canciones_animales = { "brr, fiu, cric-cric, brrah", "pep, birip, trri-trri, croac", "bri-bri, plop, cric-cric, brrah" };
+
+        string Sonido;
+
+        string Cancion;
+
+        public Canciones()
+        {
+            Sonido = "";
+            Cancion = "";
+        }
+
+        public void Obtener_sonido()
+        {
+            do
+            {
+
+            } while (!Ingresar_sonido());
+        }
+
+        public void Mostrar_resto_del_sonido()
+        {
+            Console.Write(Resto_del_sonido());
+        }
+
+        private bool Ingresar_sonido()
+        {
+            Console.Write("Ingrese el sonido: ");
+            Sonido = Console.ReadLine();
+            return Existe_el_sonido();
+        }
+
+        private bool Existe_el_sonido()
+        {
+            for (int c = 0; c < Canciones_animales.Length; c++)
+            {
+                if ((", " + Canciones_animales[c] + ",").Contains(", " + Sonido + ","))
+                {
+                    Cancion = Canciones_animales[c];
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private string Resto_del_sonido()
+        {
+            int Desde = Cancion.IndexOf(Sonido) + (Sonido.Length) + 1;
+            if (Cancion.Length - Desde > 0)
+                return Cancion.Substring(Desde, Cancion.Length - Desde).Trim();
+            else
+                return "";
+        }
+
+    }
+}
---
+++ Sonidos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Canciones Lago_musical = new Canciones();
+            Lago_musical.Obtener_sonido();
+            Lago_musical.Mostrar_resto_del_sonido();
+            Console.Read();
+        }
+
+    }
+}
+
